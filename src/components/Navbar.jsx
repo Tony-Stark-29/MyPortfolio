47a8cc8710320a4b67d701889209fa09b774e794
Santhosh Kumar P <tony.antonystark123@gmail.com>
2023-10-27 07:21:17 +0530

    Navbar Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/js/bootstrap.bundle'
 import { NavigationContext} from "../context/NavigationContext";
  
  
 export const Navbar = () => {
 
   const {currPage,setPage}=useContext(NavigationContext);
+
+  const [navBarCollapse,setCollapse]=useState(true);
 
   console.log(currPage);
  
@@ -15,14 +18,14 @@ export const Navbar = () => {
     <header className="navigation-header">
       <nav className="navbar navbar-expand-md navbar-dark px-3">
         <div className="navbar-brand logo">San</div>
-        <button className="navbar-toggler"  data-toggle="collapse" data-target="mynav">
-        <i className="fa-solid fa-bars" style={{color:'#ffffff',fontSize:"1.5em"}}></i>
-        </button>
-        <div className="collapse navbar-collapse justify-content-end " id='mynav'>
-             <Link to='/' className="nav-item nav-link px-lg-3 px-xl-5" onClick={()=>{setPage("Home")}}>Home</Link>
-             <Link to='/Education' className="nav-item nav-link px-lg-3 px-xl-5"  onClick={()=>{setPage("Education")}}>Education</Link>
-             <Link to='/MyWork' className="nav-item nav-link px-lg-3 px-xl-5"  onClick={()=>{setPage("MyWork")}}>My Work</Link>
-             <Link to='/ContactMe' className="nav-item nav-link px-lg-3 px-xl-5"  onClick={()=>{setPage("ContactMe")}}>Contact Me</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggler="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={()=>{setCollapse(!navBarCollapse)}}>
+    <span className="navbar-toggler-icon"></span>
+  </button>
+        <div className={navBarCollapse===false?"collapse navbar-collapse justify-content-end":"navbar-collapse justify-content-end"}  >
+             <Link to='/' className="nav-item nav-link px-lg-3 px-xl-5 my-nav-link" onClick={()=>{setPage("Home")}}>Home</Link>
+             <Link to='/Education' className="nav-item nav-link px-lg-3 px-xl-5 my-nav-link"  onClick={()=>{setPage("Education")}}>Education</Link>
+             <Link to='/MyWork' className="nav-item nav-link px-lg-3 px-xl-5 my-nav-link"  onClick={()=>{setPage("MyWork")}}>My Work</Link>
+             <Link to='/ContactMe' className="nav-item nav-link px-lg-3 px-xl-5 my-nav-link"  onClick={()=>{setPage("ContactMe")}}>Contact Me</Link>
         </div>
       
       </nav>
