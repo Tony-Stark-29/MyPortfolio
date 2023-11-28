57aea78a758b102be87b73fb65a8329913ac5825
Santhosh Kumar P <tony.antonystark123@gmail.com>
2023-11-28 20:42:22 +0530

    Added projects

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,6 +5,7 @@ import emailjs from "@emailjs/browser";
 import contact from "../css/ContactMe.module.css";
 import { FormPopUpContext } from "../context/FormPopUpContext";
 import { Link } from "react-router-dom";
+ 
 
 export const ContactForm = () => {
   const contactForm = useRef();
@@ -12,7 +13,7 @@ export const ContactForm = () => {
   const { setShowContactForm } = useContext(FormPopUpContext);
 
   //console.log(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID,contactForm.current,process.env.REACT_APP_PUBLIC_KEY);
-
+ 
   const emailMe = (e) => {
     e.preventDefault();
     console.log(process.env.REACT_APP_SERVICE_ID);
@@ -26,6 +27,7 @@ export const ContactForm = () => {
       )
       .then(
         (result) => {
+          alert.show();
           console.log(result.text);
         },
         (error) => {
@@ -59,28 +61,32 @@ export const ContactForm = () => {
             onSubmit={emailMe}
           >
             <h3 className="text-light">Write to me</h3>
+            <label htmlFor="sender_name">Name</label>
             <input
               type="text"
               name="sender_name"
-              defaultValue={"Name"}
+              id="sender_name"
               className={`col-sm-12 my-2 ${contact.contact_input}`}
             />
+            <label htmlFor="sender_email">Email Id</label>
             <input
               type="email"
               name="sender_email"
-              defaultValue={"Email Id"}
+              id="sender_email"
               className={`col-sm-12 my-2 ${contact.contact_input}`}
             />
+            <label htmlFor="sender_contact">Contact</label>
             <input
               type="phone"
               name="sender_contact"
-              defaultValue={"Contact"}
+              id="sender_contact"
               className={`col-sm-12 my-2 ${contact.contact_input}`}
             />
+            <label htmlFor="sender_message">Description</label>
             <textarea
               type="text"
               name="sender_message"
-              defaultValue={"Description"}
+              id="sender_message"
               className={`col-sm-12 my-2 ${contact.contact_input}`}
             />
             <button type="submit" className="btn btn-primary m-2">
--- src/pages/MyWorks.jsx
+++ src/pages/MyWorks.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { GoBackBtn } from '../components/GoBackBtn'
+import React from "react";
+import { GoBackBtn } from "../components/GoBackBtn";
 import mywork from "../css/MyWork.module.css";
+import my_portfolio_img from "../resources/project-screenshots/my_portfolio.png";
+
 
 export const MyWorks = () => {
   return (
-    <div className={mywork.container}>
-       <GoBackBtn></GoBackBtn>
-    </div>
-  )
-}
+    <>
+      <GoBackBtn></GoBackBtn>
+      <section className={mywork.container}>
+        <div className={mywork.myprojects}>
+          <div className={mywork.project}>
+          <div className={mywork.project_title}>My Portfolio</div>
+              <div className={mywork.project_img}>
+                <img src={my_portfolio_img} alt="My Portfolio" />
+              </div>
+              <div className={mywork.project_link}>
+                <button> Source <i class="fa-duotone fa-forward"></i>  </button>
+              </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
